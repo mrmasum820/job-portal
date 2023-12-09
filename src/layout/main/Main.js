a8fd2170240b8b76394a1589c307fed032089e19
MR Masum <mrmasum820@gmail.com>
2023-12-10 03:26:20 +0600

    deployed client side

--- src/layout/main/Main.js
+++ src/layout/main/Main.js
@@ -9,8 +9,8 @@ const Main = () => {
     <div>
       <Navbar />
       <div
-        className={` ${pathname === "/" ? "max-w-[1280px] px-10" : "max-w-7xl"
-          } max-w-[1280px] mx-auto`}
+        className={` ${pathname === "/" ? "max-w-[1480px] px-10" : "max-w-7xl"
+          } max-w-[1480px] mx-auto`}
       >
         <Outlet />
       </div>
